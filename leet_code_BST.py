6fe2ceee153391fb2fe04b153fb3d4e46c29f08d
Rob Marshall <rob.marshall17@gmail.com>
2020-12-29 21:58:44 -0500

    Add vim .swp files to .gitignore leet code BST stuff

--- leet_code_BST.py
+++ leet_code_BST.py
@@ -7,6 +7,7 @@ from collections import deque
 Utility classes and functions for Leet Code Binary Search Trees.
 """
 
+
 class TreeNode:
     def __init__(self, x):
         self.val = x
@@ -19,12 +20,40 @@ class TreeNode:
         ret += " right: %s" % ("None" if self.right is None else str(self.right.val))
         return ret
 
+
+def breadth_first_search(tree):
+    levels = []
+    tree_depth = get_tree_depth(tree)
+    for i in range(tree_depth):
+        levels.append([None] * (2 ** i))
+    # print(levels)
+    queue = deque()
+    queue.append(tree)
+    level = 0
+    while level < tree_depth:
+        size = len(queue)
+        # print("levels[level=%d]=%s size=%d" % (level, levels[level], size))
+        for i in range(size):
+            current_node = queue.popleft()
+            if current_node is None:
+                queue.append(None)
+                queue.append(None)
+            else:
+                queue.append(current_node.left)
+                queue.append(current_node.right)
+            levels[level][i] = current_node
+        # print("End of level %d: %s" % (level, levels[level]))
+        level += 1
+    return levels
+
+
 def get_tree_depth(root: TreeNode) -> int:
     if root is None:
         return 0
     left = get_tree_depth(root.left)
     right = get_tree_depth(root.right)
     return max(left, right) + 1
+
 
 # The list of values being provided could be ints, strings, None...
 def build_tree(nodes: List[Any]) -> TreeNode:
@@ -54,7 +83,7 @@ def build_tree(nodes: List[Any]) -> TreeNode:
             left_node = TreeNode(nodes[i])
             queue.append(left_node)
             current_node.left = left_node
-        if nodes[i+1] is not None:
+        if i + 1 < len(nodes) and nodes[i+1] is not None:
             right_node = TreeNode(nodes[i+1])
             queue.append(right_node)
             current_node.right = right_node
@@ -63,49 +92,25 @@ def build_tree(nodes: List[Any]) -> TreeNode:
 
 def print_tree(tree):
     """
-    For the print tree we do a level order traversal. We may want to decide how deep the
-    tree goes first, however, just so that we can print it correctly.
-    :param tree:
-    Determine the depth of the tree
-    The number of nodes per level increases as a power of 2
-    Each node needs to be centered in X spaces
-    The maximum width of the output will be 2**(tree depth - 1) * width of each node
-    Perform a level order traversal of the tree
-    create a queue
-    add the tree, i.e. the first node, to the queue
-    initialize the level variable
-    while the queue is not empty
-        pop next node from queue
-        print value centered in string dependent on level
-    :return: None
+    For print tree, run the breadth_first_search on the tree which will return
+    a list contains a list of nodes for each level. The basic idea is that the
+    last level will have an initial single space and each node will be separated
+    by a space. For the second to last level, the level will start with 2 spaces
+    and each node will be separated by 4. From there everything will double with
+    each level.
+
+    Pseudocode:
+    get the levels from breadth_first_search()
+    Count down in reverse order multiplying the initial spaces and spaces between nodes
     """
-    node_width = 5
-    node_fmt = '{:^%d}' % node_width
-    tree_depth = get_tree_depth(tree) - 1
+    levels = breadth_first_search(tree)
     level = 0
-    queue = deque()
-    queue.append(tree)
-    while level <= tree_depth:
-        current_level = []
-        for _ in range(level * tree_depth if level > 0 else 1):
-            current_level.append(queue.popleft())
-        filler = (' '*node_width)*(tree_depth - level)
+    for i in range(len(levels) - 1, -1 , -1):
+        between = ' '*(2*(2**i))
+        output = ' '*(2**i)
+        # print('levels[%d]=%s' % (level, levels[level]))
+        for node in levels[level]:
+            output += str(node.val) if node is not None else ' '
+            output += between
+        print(output)
         level += 1
-        # print('current level: %s' % current_level)
-        print(filler,end='')
-        for current_node in current_level:
-            if current_node is None:
-                print(' '*node_width,end='')
-            else:
-                print(node_fmt.format(current_node.val),end='')
-            if current_node is not None:
-                queue.append(current_node.left)
-                queue.append(current_node.right)
-        print()
-            # current_node =
-            # for _ in range(tree_depth - level):
-            #     print('-' * node_width, end='')
-            # level += 1
-            # print(node_fmt.format(current_node.val if current_node != '' else ''))
-            # queue.append(current_node.left if current_node.left is not None else '')
-            # queue.append(current_node.right if current_node.right is not None else '')
